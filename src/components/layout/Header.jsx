a4ff6d09a9aab969ac0bd35113b3dd5e956c1301
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-03-01 23:19:59 -0500

    Add Header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,23 @@
+import { useThemeContext } from "../../context/ThemeContext";
+
+export default function Header() {
+	const { isDark, toggleTheme } = useThemeContext();
+
+	return (
+		<header>
+			<nav>
+				<h1 className="title">Where in the world?</h1>
+
+				<button
+					id="themeBtn"
+					type="button"
+					role="switch"
+					aria-checked={isDark}
+					onClick={toggleTheme}
+				>
+					{isDark ? "Light Mode" : "Dark Mode"}
+				</button>
+			</nav>
+		</header>
+	);
+}
