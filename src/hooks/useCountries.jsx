177e5fbed2880315483e41404bf0006198d0eb5e
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-03-01 22:26:58 -0500

    Add useCountries hook

---
+++ src/hooks/useCountries.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState, useRef } from "react";
+
+export function useCountries() {
+	const [countries, setCountries] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+	const isMountedRef = useRef(true); // track a changeable value > reference does not cause a re-render when updated
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				// destructured > Promise.all to fetch both dataset in parallel > more efficient
+				const [responseCountries, responseCodes] = await Promise.all([fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags,subregion,languages,tld,currencies,borders"), fetch("https://restcountries.com/v3.1/all?fields=name,cca3")]);
+
+				if (!responseCountries.ok) {
+					throw new Error(`HTTP error: ${responseCountries.status}`);
+				}
+				if (!responseCodes.ok) {
+					throw new Error(`HTTP error: ${responseCodes.status}`);
+				}
+
+				const countryCodeMap = new Map();
+				const dataCountries = await responseCountries.json();
+				const dataCodes = await responseCodes.json();
+
+				dataCodes.forEach((country) => {
+					countryCodeMap.set(country.cca3, country.name.common);
+				});
+
+				// add country codes to full country objects
+				const countriesWithCodes = dataCountries.map((country) => {
+					const match = dataCodes.find((codeCountry) => codeCountry.name.common === country.name.common);
+
+					return {
+						...country,
+						cca3: match?.cca3,
+					};
+				});
+
+				if (isMountedRef.current) {
+					setCountries(dataCountries);
+					setLoading(false);
+				}
+			} catch (error) {
+				if (isMountedRef.current) {
+					console.error("Failed to fetch countries:", error);
+					setError(error.message);
+					setLoading(false);
+				}
+			}
+		}
+
+		fetchData();
+		return () => (isMountedRef.current = false);
+	}, []);
+
+	return { countries, loading, error };
+}
